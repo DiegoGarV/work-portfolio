8c759d7e739229cb0dfc6b8d306baeac304c48b0
DiegoGarV <dgv3105@gmail.com>
2024-05-29 01:10:51 -0600

    Estructura inicial

---
+++ portafolio/src/components/ground/Technologies.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Technologies.css'
+
+const Technologies = () => {
+    return (
+        <section id="technologies" className="technologies">
+            <h2>Projects</h2>
+            <div className="tech-list">
+                <div className="tech">
+                    <h3>Project 1</h3>
+                    <p>Description of project 1.</p>
+                </div>
+                <div className="tech">
+                    <h3>Project 2</h3>
+                    <p>Description of project 2.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Technologies
---
+++ portafolio/src/components/sky/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './AboutMe.css'
+
+const AboutMe = () => {
+    return (
+        <section id="about" className="about">
+            <h2>About Me</h2>
+            <p>This is where you tell about yourself.</p>
+        </section>
+    )
+}
+
+export default AboutMe
